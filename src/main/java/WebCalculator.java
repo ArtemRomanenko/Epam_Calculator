3787e5374c2b2e7ca52701bf4d134b4dd2d1bb55
ArtemRomanenko <@>
2020-05-31 16:49:31 +0200

    ApiCalculator and Unit tests

--- src/main/java/WebCalculator.java
+++ src/main/java/WebCalculator.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -90,6 +91,14 @@ public class WebCalculator implements CalculatorInterface {
         inputField.clear();
         webCalculation(x);
         divideButton.click();
+        try {
+            if (y == 0.0) {
+                throw new ArithmeticException();
+            }
+        } catch (Exception e) {
+            System.out.println("You cannot divide by zero");
+            return null;
+        }
         webCalculation(y);
         calcButton.click();
         try {
